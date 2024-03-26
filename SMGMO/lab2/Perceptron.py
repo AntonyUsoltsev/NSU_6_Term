b8b205f0d6e5850d77487a00cfa84047bafab6d0
AntonyUsoltsev <usoltsev_anton1@mail.ru>
2024-03-26 17:56:54 +0700

    some new methods, add x^2 and y^2 to input params

--- SMGMO/lab2/Perceptron.py
+++ SMGMO/lab2/Perceptron.py
@@ -3,21 +3,33 @@ import numpy as np
 
 
 def sigmoid(x):
-    return 1 / (1 + math.exp(-x))
+    return 1 / (1 + np.exp(-x))
 
 
-def classification(train: np.ndarray, epochs, ed_coef, K, delta, type):
+def classification(train: np.ndarray, epochs, ed_coef, K, delta, type, weigth_size):
     if type == "step":
-        return step_classification(train, epochs, ed_coef)
+        return step_classification(train, epochs, ed_coef, weigth_size)
     elif type == "sigm":
-        return sigm_classification(train, epochs, ed_coef, K, delta)
+        return sigm_classification(train, epochs, ed_coef, K, delta, weigth_size)
+    else:
+        raise ValueError("Invalid classification type")
 
 
-def step_classification(train: np.ndarray, epochs, ed_coef):
-    W = np.zeros(len(train[0]))
+def prepare_data(train: np.ndarray, feature_size):
     train_set = train[:, :2]
     class_data = train[:, 2]
-    train_data = np.hstack((np.ones(len(train_set)).reshape(-1, 1), train_set))
+    if feature_size == 3:  # Получим линию
+        train_data = np.hstack((np.ones(len(train_set)).reshape(-1, 1), train_set))
+    elif feature_size == 5:  # Получим кривую
+        train_data = np.hstack((np.ones(len(train_set)).reshape(-1, 1), train_set, train_set ** 2))
+    else:
+        raise ValueError("Invalid weight vector dimension")
+    return train_data, class_data
+
+
+def step_classification(train: np.ndarray, epochs, ed_coef, feature_size):
+    train_data, class_data = prepare_data(train, feature_size)
+    W = np.zeros(len(train_data[0]))
     for _ in range(epochs):
         for i in range(len(train_data)):
             res = W @ train_data[i]
@@ -29,15 +41,13 @@ def step_classification(train: np.ndarray, epochs, ed_coef):
     return W
 
 
-def sigm_classification(train: np.ndarray, epochs, ed_coef, K, delta):
-    train_set = train[:, :2]
-    class_data = train[:, 2]
-    train_data = np.hstack((np.ones(len(train_set)).reshape(-1, 1), train_set))
+def sigm_classification(train: np.ndarray, epochs, ed_coef, K, delta, feature_size):
+    train_data, class_data = prepare_data(train, feature_size)
     W_min = []
     min_error = math.inf
     for _ in range(epochs):
-        W = np.random.rand(len(train[0]))
-        grad = np.zeros(len(train[0]))
+        W = np.random.rand(len(train_data[0]))
+        grad = np.zeros(len(train_data[0]))
 
         for k in range(K):  # цикл градиентного спуска
             error = 0
--- SMGMO/lab2/main.py
+++ SMGMO/lab2/main.py
@@ -1,8 +1,8 @@
 import time
 from sklearn.metrics import confusion_matrix
-import Generator as generator
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+import Generator as generator
 import Perceptron as perceptron
 
 
@@ -14,63 +14,75 @@ def draw_set(set_to_draw):
     plt.scatter(class_1[:, 0], class_1[:, 1], label='Class 1', alpha=0.5)
 
 
-def draw_separate_line(coef_matr):
-    k, a, b = coef_matr[0], coef_matr[1], coef_matr[2]
-    if b != 0:
-        x_values = np.linspace(-5, 5, 2)
-        y_values = (-k - a * x_values) / b
-    else:
-        y_values = np.linspace(-5, 5, 2)
-        x_values = -k / a
-    plt.plot(x_values, y_values)
-    plt.xlabel('X')
-    plt.ylabel('Y')
+def draw_separate_curve(coef_matr, border_set):
+    coef_matr = list(coef_matr) + [0] * (5 - len(coef_matr))
+    k, a, b, c, d = coef_matr[0], coef_matr[1], coef_matr[2], coef_matr[3], coef_matr[4]
+
+    x_min, x_max = np.min(border_set[:, 0]) - 0.3, np.max(border_set[:, 0]) + 0.3
+    y_min, y_max = np.min(border_set[:, 1]) - 0.3, np.max(border_set[:, 1]) + 0.3
+
+    x = np.linspace(x_min, x_max, 100)
+    y = np.linspace(y_min, y_max, 100)
+    x, y = np.meshgrid(x, y)
+
+    z = k + a * x + b * y + c * x ** 2 + d * y ** 2
+
+    plt.contour(x, y, z, levels=[0], colors='k')
     plt.axis('equal')
-    plt.legend()
-    plt.ylim(-3, 3)
 
 
-def class_split(data, coefs):
+def class_split(data, coefs, feature_size):
     train_set = data[:, :2]
-    train_data = np.hstack((np.ones(len(train_set)).reshape(-1, 1), train_set))
+    train_data, class_data = perceptron.prepare_data(data, feature_size)
     return np.hstack((train_set, (train_data @ coefs > 0.0).astype(int).reshape(-1, 1)))
 
 
-def classification(train_set, epochs, ed_coef, K, delta, type):
+def classification(train_set, epochs, ed_coef, K, delta, type, feature_size):
     start_time = time.time()
-    W = perceptron.classification(train_set, epochs, ed_coef, K, delta, type)
+    W = perceptron.classification(train_set, epochs, ed_coef, K, delta, type, feature_size)
     end_time = time.time()
     execution_time = end_time - start_time
     print(f"Execution time for {type}: {round(execution_time, 3)} seconds")
 
-    set_after_class = class_split(train_set, W)
+    set_after_class = class_split(train_set, W, feature_size)
     # INFO: (i, j) - i-ый класс распознан как j
     cm = confusion_matrix(train_set[:, 2], set_after_class[:, 2])
     print(cm)
-
     draw_set(set_after_class)
-    draw_separate_line(W)
+
+    draw_separate_curve(W, set_after_class)
     plt.title(type)
-    plt.show()
 
 
 def main():
     num_points = 100
     noise = 0
-    epochs = 1000
+    epochs = 100
     ed_coef = 0.5
 
     K = 10  # K шагов градиентного спуска
     delta = 0.05  # Порог точности локального минимума
 
-    # INFO: train_set = (x,y,c) c -- класс: 1 или 0
+    """ INFO: 
+        :returns [(x,y,c)], где x и y - координаты,  c - класс: 1 или 0
+        :param
+            type = "circle" для генерации кольцевой выборки
+                 = "gauss" для генерации двух Гауссовых множеств
+    """
     train_set = generator.generate(num_points, noise, "gauss")
-
     draw_set(train_set)
-    plt.show()
 
-    classification(train_set, epochs, ed_coef, K, delta, "sigm")
-    classification(train_set, epochs, ed_coef, K, delta, "step")
+    """ INFO: 
+        :param
+            type = "step" для перцептрона со ступенчатой функцией активации
+                 = "sigm" для перцептрона с сигмоидальной функцией активации и обратным распространением ошибки
+             
+            feature_size = 3 для тренировки на наборах вида [1, x, y]
+                         = 5 для тренировки на наборах вида [1, x, y, x^2, y^2]
+    """
+    classification(train_set, epochs, ed_coef, K, delta, "sigm", 3)
+
+    plt.show()
 
 
 main()
