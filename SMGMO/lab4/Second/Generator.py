29f2f06d85408396442d303826f9be8aa8bfe4ab
AntonyUsoltsev <usoltsev_anton1@mail.ru>
2024-04-25 23:54:42 +0700

    some fix

--- SMGMO/lab4/Second/Generator.py
+++ SMGMO/lab4/Second/Generator.py
@@ -1,21 +1,36 @@
 import numpy as np
 
 
-def polynome(a, b, c, d, x):
-    return a * x ** 3 + b * x ** 2 + c * x + d
+class DataGenerator:
+    def __init__(self, generation_type, coefs=None):
+        self.generation_type = generation_type
+        self.coefs = coefs
 
+    def polynome(self, x):
+        degree = len(self.coefs) - 1
+        result = sum(coeff * x ** (degree - i) for i, coeff in enumerate(self.coefs))
+        return result
 
-def sinus(x):
-    return x * np.sin(2 * np.pi * x)
+    def sinus(self, x):
+        return x * np.sin(2 * np.pi * x)
 
+    def generate(self, N, noise):
+        x = np.random.uniform(-1, 1, N)
+        eps = np.random.uniform(-noise, noise, N)
+        if self.generation_type == "sin":
+            y = self.sinus(x) + eps
+        elif self.generation_type == "poly":
+            y = self.polynome(x) + eps
+        else:
+            raise ValueError("Invalid generation type")
+        return x, y
 
-def generate(a, b, c, d, N, noise, func):
-    x = np.random.uniform(-1, 1, N)
-    eps = np.random.uniform(-noise, noise, N)
-    if func == "sin":
-        y = sinus(x) + eps
-    elif func == "poly":
-        y = polynome(a, b, c, d, x) + eps
-    else:
-        raise ValueError("Invalid generation type")
-    return x, y
+    def generate_function(self):
+        x = np.arange(-1, 1, 0.01)
+        if self.generation_type == "sin":
+            y = self.sinus(x)
+        elif self.generation_type == "poly":
+            y = self.polynome(x)
+        else:
+            raise ValueError("Invalid function generation type")
+        return x, y
--- SMGMO/lab4/Second/main.py
+++ SMGMO/lab4/Second/main.py
@@ -4,13 +4,15 @@ import torch.optim as optim
 import matplotlib.pyplot as plt
 import numpy as np
 from Perceptron import MLP
-import Generator as generator
+from Generator import DataGenerator
 from sklearn.model_selection import KFold
 
 
 def draw_regression_result(x_t, y_t, x_p, y_p):
-    plt.scatter(x_t, y_t, label="Target")
-    plt.scatter(x_p, y_p, label="Predicted")
+    x_f, y_f = generator.generate_function()
+    plt.plot(x_f, y_f, label="Target Function")
+    plt.plot(x_p, y_p, label="Predicted", color="red")
+    plt.scatter(x_t, y_t, label="Dataset")
     plt.xlabel('x')
     plt.ylabel('y')
     plt.legend()
@@ -44,13 +46,14 @@ def cross_validation(input_size, hidden_sizes, output_size, activation, x_data, 
     for train_index, test_index in kf.split(x_data):
         x_train, x_test = x_data[train_index], x_data[test_index]
         y_train, y_test = y_data[train_index], y_data[test_index]
+
         x_train_tensor = torch.tensor(x_train, dtype=torch.float32).unsqueeze(1)
         y_train_tensor = torch.tensor(y_train, dtype=torch.float32)
         x_test_tensor = torch.tensor(x_test, dtype=torch.float32).unsqueeze(1)
         y_test_tensor = torch.tensor(y_test, dtype=torch.float32)
 
         model = MLP(input_size, hidden_sizes, output_size, activation)
-        model_predictions = train_model(model, x_train_tensor, y_train_tensor, epochs, lr)
+        train_model(model, x_train_tensor, y_train_tensor, epochs, lr)
 
         with torch.no_grad():
             outputs = model(x_test_tensor)
@@ -58,9 +61,16 @@ def cross_validation(input_size, hidden_sizes, output_size, activation, x_data, 
             losses.append(loss)
             if loss < min_loss:
                 min_loss = loss
-                best_train = x_train_tensor
-                best_predictions = model_predictions.numpy()
-            # draw_regression_result(x_train, y_train, x_train, model_predictions.numpy())
+                # best_train = np.concatenate((x_train, x_test))
+                # best_predictions = np.concatenate((model_predictions.numpy(), outputs.numpy()))
+                best_train = np.arange(-1, 1, 0.01)
+                contin_outputs = model(torch.tensor(np.arange(-1, 1, 0.01), dtype=torch.float32).unsqueeze(1))
+                best_predictions = contin_outputs.numpy()
+            draw_regression_result(x_train,
+                                   y_train,
+                                   np.arange(-1, 1, 0.01),
+                                   model(
+                                       torch.tensor(np.arange(-1, 1, 0.01), dtype=torch.float32).unsqueeze(1)).numpy())
 
         print("Cross validation iteration end\n")
 
@@ -72,20 +82,17 @@ def cross_validation(input_size, hidden_sizes, output_size, activation, x_data, 
 
 
 def main():
-    num_points = 200
-    noise = 0.0
+    num_points = 10
+    noise = 0.2
     epochs = 2000
-    learning_rate = 0.03
+    learning_rate = 0.003
     cross_validation_count = 5
-
-    # Задание данных для обучения
-    x_train, y_train = generator.generate(-4, 2, 7, -2, num_points, noise, "poly")
+    x_train, y_train = generator.generate(num_points, noise)
 
     # Задание параметров модели и обучение
     input_size = 1
     output_size = 1
-    hidden_sizes = [8]  # Количество нейронов в скрытых слоях
-
+    hidden_sizes = [10, 15, 5, 4]  # Количество нейронов в скрытых слоях
     activation_functions = [nn.Sigmoid, nn.Tanh, nn.ReLU]  # Список функций активации
 
     for activation in activation_functions:
@@ -95,5 +102,9 @@ def main():
                                                         cross_validation_count, epochs, learning_rate)
         draw_regression_result(x_train, y_train, best_train, best_predictions)
 
+
+function_type = "sin"
+poly_coeffs = [-4, 2, 7, -2]
+generator = DataGenerator(function_type, poly_coeffs)
 
 main()
